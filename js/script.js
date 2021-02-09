306ca2d4a7460d3818b67791e2f981f1c5b57426
Salinas95 <samuelsalinas95@gmail.com>
2021-02-09 04:03:54 +0100

    contenido4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
 
 
+/************************** PIXABAY **************************/
+const contenido4=document.getElementById("contenido4");
+const galeria=document.getElementById("galeria");
+const img=document.getElementById("img");
+
+
+const mostrarPixabay=(imagenes)=>{
+    for(imagen of imagenes.hits){
+        //console.log("hola");
+        console.log(imagen);
+
+        /* const hola=document.createElement("div");
+        hola.className+="article__contenido4"; */
+
+        const img=document.createElement("img");
+        img.className+="contenido4__img";
+        img.src=imagen.webformatURL;
+        galeria.append(img);
+    } 
+}
+
+const buscarPixabay=()=>{
+    fetch("https://pixabay.com/api/?key=20164793-b3f95408f84ff41044484cc56&q=new+york&image_type=photo")
+        .then(imagenes => imagenes.json())
+        .then(imagenes => mostrarPixabay(imagenes))
+}
+
+document.addEventListener("DOMContentLoaded",buscarPixabay);
+/************************** PIXABAY **************************/
+
+
+
+
+
+
+
 
 
 
